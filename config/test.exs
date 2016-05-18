d7ed0d9e330f8602dd4df9dbc41f658dafbdec31
Martin Feckie <mfeckie@gmail.com>
2016-05-18 22:14:47 +0800

    Update config for environments

---
+++ config/test.exs
@@ -0,0 +1,5 @@
+use Mix.Config
+
+config :phoenix_frontend_deploys,
+  asset_directory: System.cwd <> "/test/frontend_assets",
+  live_filename: "live"
--- mix.exs
+++ mix.exs
@@ -17,6 +17,7 @@ defmodule PhoenixFrontendDeploys.Mixfile do
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
