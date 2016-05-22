c4a77d5f061e24331f335f4e21b321b381fb5606
Martin Feckie <mfeckie@gmail.com>
2016-05-22 22:38:07 +0800

    Make the TestApp match the new API

--- test/support/test_app.ex
+++ test/support/test_app.ex
@@ -1,10 +1,18 @@
 defmodule TestApp.Router do
   use Phoenix.Router
 
-  forward "/frontends", PhoenixFrontendDeploys.Web
+  get "/frontends", PhoenixFrontendDeploys.FrontendController, :index
+
+
+  get "/revisions", TestController, :index
+  post "revisions/activate", TestController, :activate
 end
 
 defmodule TestApp do
   use Phoenix.Endpoint, otp_app: :test_app
   plug TestApp.Router
+end
+
+defmodule TestController do
+  use PhoenixFrontendDeploys.RevisionsController
 end
