7b4a2bcf2fee4e196eaeab45d2a8264e88989fc6
Martin Feckie <mfeckie@gmail.com>
2016-05-22 22:35:57 +0800

    Add activate revision action

--- admin_ui/phoenix_frontend_deploys/app/components/revision-list/component.js
+++ admin_ui/phoenix_frontend_deploys/app/components/revision-list/component.js
@@ -2,5 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'table',
-  elementId: 'revision-list'
+  elementId: 'revision-list',
+  ajax: Ember.inject.service(),
+  actions: {
+    activateRevision(revision) {
+      const ajax = this.get('ajax');
+      ajax.post('/test/api/revisions/activate', {
+        data: {
+          revision: revision.id
+        }
+      });
+    }
+  }
 });
