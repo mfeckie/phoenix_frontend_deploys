49637a88a4a80f71f10d86d874cc1b7a58595363
Martin Feckie <mfeckie@gmail.com>
2016-05-22 22:37:02 +0800

    Add some phoenix config to all JSON encoding

--- config/test.exs
+++ config/test.exs
@@ -3,3 +3,7 @@ use Mix.Config
 config :phoenix_frontend_deploys,
   asset_directory: System.cwd <> "/test/frontend_assets",
   live_filename: "live"
+
+# This is so we can test the controllers
+config :phoenix,
+  format_encoders: [json: Poison]
